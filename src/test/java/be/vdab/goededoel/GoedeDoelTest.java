b29490fefe6a9646367842382b0e1a31fb362cd8
Jakub <jakub.majewski@vdabcampus.be>
2024-08-13 15:00:54 +0200

    fixing

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import be.vdab.goededoel.GoedeDoel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
 
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
@@ -22,7 +23,7 @@ class GoedeDoelTest {
 
 	@Test
 	void eenNieuwDoelHeeftNogGeenOpbrengst() {
-		// Dit is een foutgevende test
-		Assertions.assertThat(doel.getOpgebracht()).isZero();
+		Assertions.assertThat(doel.getOpbrengst()).isZero();
+
 	}
 }
